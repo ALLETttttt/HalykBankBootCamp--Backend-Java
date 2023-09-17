6ec36903c3ce498019a4dccd0c5a2bf7a54ecc5c
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-09-17 21:18:10 +0600

    SSIU

--- HW1/src/Ass_1/ex_5.java
+++ HW1/src/Ass_1/ex_5.java
@@ -1,7 +1,19 @@
 package Ass_1;
 
+import java.util.Scanner;
+
 public class ex_5 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        if (n >= 1 && n <= 12) {
+            if (n <= 2 || n == 12) System.out.println("Winter");
+            else if (n < 6) System.out.println("Spring");
+            else if (n < 9) System.out.println("Summer");
+            else System.out.println("Autumn");
+        }
+        else {
+            System.out.println("You are a mistake");
+        }
     }
 }
