8002716a6a965879144930578deaaf0645ce8e38
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-09-17 16:48:29 +0600

    SIU

---
+++ HW1/src/Ass_1/ex_3.java
@@ -0,0 +1,13 @@
+package Ass_1;
+
+import java.util.Scanner;
+
+public class ex_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int first_number = input.nextInt();
+        int second_number = input.nextInt();
+        int third_number = input.nextInt();
+        System.out.println(String.valueOf(first_number) + String.valueOf(second_number) + String.valueOf(third_number));
+    }
+}
