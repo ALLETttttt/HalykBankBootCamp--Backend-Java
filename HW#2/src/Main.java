9293aeb37c1f7782b0c6bd74a592a69790ff5fad
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-09-18 21:07:55 +0600

    SIU

--- HW#2/src/Main.java
+++ HW#2/src/Main.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
+        // Press Opt+Enter with youlr caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
 
