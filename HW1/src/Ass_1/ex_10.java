03d55252f7e50dcbf618a12aa8119310df205dee
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-09-18 20:55:35 +0600

    SIU

---
+++ HW1/src/Ass_1/ex_10.java
@@ -0,0 +1,9 @@
+package Ass_1;
+
+public class ex_10 {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 3, -5, 9, -5, -234, -44, 23, 0, 23, 0, 0, 0, -34, -7, 10000, 0};
+        int[] arrPos = {}, arrNeg = {}, arrEven = {}, arrOdd = {};
+
+    }
+}
