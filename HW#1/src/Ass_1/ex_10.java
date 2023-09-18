45e7180d3d3ad303e4f6b93fa6b869d99bdc69ed
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-09-18 23:30:21 +0600

    SIU

--- HW#1/src/Ass_1/ex_10.java
+++ HW#1/src/Ass_1/ex_10.java
@@ -3,7 +3,88 @@ package Ass_1;
 public class ex_10 {
     public static void main(String[] args) {
         int[] arr = {1, 4, 3, -5, 9, -5, -234, -44, 23, 0, 23, 0, 0, 0, -34, -7, 10000, 0};
-        int[] arrPos = {}, arrNeg = {}, arrEven = {}, arrOdd = {};
+        int[] arrPos = filterPos(arr),
+                arrNeg = filterNeg(arr),
+                arrEven = filterEven(arr),
+                arrOdd = filterOdd(arr);
+        printArray(arrPos);
+        printArray(arrNeg);
+        printArray(arrEven);
+        printArray(arrOdd);
+    }
 
+    public static int[] filterEven(int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                count++;
+            }
+        }
+        int[] result = new int[count];
+        int index = 0;
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                result[index++] = num;
+            }
+        }
+        return result;
+    }
+
+    public static int[] filterOdd(int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 != 0) {
+                count++;
+            }
+        }
+        int[] result = new int[count];
+        int index = 0;
+        for (int num : arr) {
+            if (num % 2 != 0) {
+                result[index++] = num;
+            }
+        }
+        return result;
+    }
+
+    public static int[] filterPos(int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num > 0) {
+                count++;
+            }
+        }
+        int[] result = new int[count];
+        int index = 0;
+        for (int num : arr) {
+            if (num > 0) {
+                result[index++] = num;
+            }
+        }
+        return result;
+    }
+
+    public static int[] filterNeg(int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num < 0) {
+                count++;
+            }
+        }
+        int[] result = new int[count];
+        int index = 0;
+        for (int num : arr) {
+            if (num < 0) {
+                result[index++] = num;
+            }
+        }
+        return result;
+    }
+
+    public static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
