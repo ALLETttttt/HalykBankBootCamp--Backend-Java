376406a47d5c9f6e2d3720f85390eec74186f7ce
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-09-21 14:14:21 +0600

    SIUUU

---
+++ HW#1/src/Ass_2/ex_5.java
@@ -0,0 +1,34 @@
+package Ass_2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ex_5 {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Random random = new Random();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(21) - 10;
+            sum += arr[i];
+        }
+        int average = sum/ arr.length;
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Average of randomly pushed elements in array: " + average);
+        int counter = 0;
+        for (int j : arr) {
+            if (j < average) {
+                counter++;
+            }
+        }
+        int[] newArr = new int[counter];
+        int index = 0;
+        for (int j : arr) {
+            if (j < average) {
+                newArr[index++] = j;
+            }
+        }
+        Arrays.sort(newArr);
+        System.out.println("New sorted array with low element than average: " + Arrays.toString(newArr));
+    }
+}
