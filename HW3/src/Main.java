cc0a41c5eb66a2471430ee009ab878965d941f84
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-09-24 17:39:36 +0600

    SIU

---
+++ HW3/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Money price = new Money(10, 50);
+
+        Product product = new Product("Chocolate Bar", price);
+
+        product.display();
+
+        product.reducePrice(3, 25);
+
+        product.display();
+    }
+}
--- HW3/src/Money.java
+++ HW3/src/Money.java
@@ -31,7 +31,62 @@ public class Money {
                 '}';
     }
 
-    public void getSum() {
-        System.out.println(dollar + "$" + cent + "Cent");
+    public void display() {
+        System.out.println(dollar + " dollars " + cent + " cents");
+    }
+
+    public void add(int dollar, int cent) {
+        this.dollar += dollar;
+        this.cent += cent;
+        if (this.cent >= 100) {
+            this.dollar += this.cent / 100;
+            this.cent %= 100;
+        }
+    }
+
+    public void subtract(int dollar, int cent) {
+        this.dollar -= dollar;
+        this.cent -= cent;
+        if (this.cent < 0) {
+            this.dollar -= 1;
+            this.cent += 100;
+        }
     }
 }
+
+class Product {
+    private String name;
+    private Money price;
+
+    public Product(String name, Money price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public void setPrice(Money price) {
+        this.price = price;
+    }
+
+    public void reducePrice(int dollar, int cent) {
+        price.subtract(dollar, cent);
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.print("Price: ");
+        price.display();
+    }
+}
+
