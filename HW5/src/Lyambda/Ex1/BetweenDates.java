a94d8959e5cfe3a81b0f31183e5d0dd61b5751ba
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-10-14 00:50:00 +0600

    SIUUU

---
+++ HW5/src/Lyambda/Ex1/BetweenDates.java
@@ -0,0 +1,8 @@
+package Lyambda.Ex1;
+
+import java.time.LocalDate;
+
+public interface BetweenDates {
+
+    int calculateBetweenDates(LocalDate date1, LocalDate date2);
+}
---
+++ HW5/src/Lyambda/Ex1/LeapYearChecker.java
@@ -0,0 +1,5 @@
+package Lyambda.Ex1;
+
+public interface LeapYearChecker {
+    boolean isLeapYear(int year);
+}
