400f2fba0f667e6e81664d3103ab6a773b097a06
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-11-10 23:29:26 +0600

    SIUU

--- HW7/src/Multithreading/MyFirstThread.java
+++ HW7/src/Multithreading/MyFirstThread.java
@@ -1,9 +1,10 @@
 package Multithreading;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class MyFirstThread {
-    private static int SIZE = 100;
+    private static int SIZE = 10;
 
     private static int arr[] = new int[SIZE];
 
@@ -12,6 +13,50 @@ public class MyFirstThread {
     private static double average;
 
     public static void main(String[] args) {
+        Thread fillArray = new Thread(MyFirstThread::fillArray);
+
+        Thread getSum = new Thread(MyFirstThread::getSum);
+
+        Thread getAverage = new Thread(MyFirstThread::getAverage);
+
+        fillArray.start();
+        try {
+            fillArray.join();
+            getSum.start();
+            getAverage.start();
+
+            getSum.join();
+            getAverage.join();
+
+            System.out.println("Исходный массив:");
+            System.out.println(Arrays.toString(arr));
+
+            System.out.println("Сумма элементов массива: " + sum);
+            System.out.println("Среднее арифметическое массива: " + average);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void fillArray() {
+        Random random = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            arr[i] = random.nextInt(100);
+        }
+    }
+
+    private static void getSum() {
+        for (int i = 0; i < SIZE; i++) {
+            sum += arr[i];
+        }
+    }
+
+    private static void getAverage() {
+        int total = 0;
+        for (int i = 0; i < SIZE; i++) {
+            total += arr[i];
+        }
+        average = (double) total / SIZE;
     }
 }
 
