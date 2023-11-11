38642b72146d45d896d4ad2c270cb752b29950f2
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-11-11 22:37:00 +0600

    SIUUUU

--- HW7/src/Multithreading/nFacPrimeThread.java
+++ HW7/src/Multithreading/nFacPrimeThread.java
@@ -7,26 +7,33 @@ import java.util.Scanner;
 
 public class nFacPrimeThread {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        String filePath = "randomNumbers.txt";
+        System.out.println("Введите название файла:");
+        String filePath = input.nextLine();
 
         Thread fillFileThread = new Thread(() -> generateRandomNumbersToFile(filePath));
         fillFileThread.start();
 
+        String factorialPath = "factorialNumbers.txt";
         ArrayList<Integer> factorialNumbers = new ArrayList<>();
         Thread findFactorialThread = new Thread(() -> findFactorial(factorialNumbers, filePath));
 
-        Thread findPrimeNumbersThread = new Thread(nFacPrimeThread::findPrimeNumbers);
+        String primePath = "primeNumbers.txt";
+        ArrayList<Integer> primeNumbers = new ArrayList<>();
+        Thread findPrimeNumbersThread = new Thread(() -> findPrimeNumbers(primeNumbers, filePath));
 
         try {
             fillFileThread.join();
 
             findFactorialThread.start();
+            findPrimeNumbersThread.start();
+
             findFactorialThread.join();
+            findPrimeNumbersThread.join();
 
-            String factorialPath = "factorialNumbers.txt";
-
-            fillFactorialNumbersToFile(factorialPath, factorialNumbers);
+            fillNumbersToFile(factorialPath, factorialNumbers);
+            fillNumbersToFile(primePath, primeNumbers);
 
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
@@ -36,14 +43,11 @@ public class nFacPrimeThread {
     private static void generateRandomNumbersToFile(String filePath) {
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
             Random random = new Random();
-
             for (int i = 0; i < 100; i++) {
                 int randomNumber = random.nextInt(10);
                 writer.write(Integer.toString(randomNumber));
                 writer.newLine();
             }
-            System.out.println("Сгенерированные числа успешно записаны в файл.");
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -56,7 +60,6 @@ public class nFacPrimeThread {
                 int number = Integer.parseInt(line);
                 factorialNumbers.add(factorial(number));
             }
-            System.out.println("Факториал чисел найден.");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -69,17 +72,40 @@ public class nFacPrimeThread {
         return number * factorial(number-1);
     }
 
-    private static void fillFactorialNumbersToFile(String filePath, ArrayList<Integer> factorialNumbers) {
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            for (int number: factorialNumbers) {
-                writer.write(Integer.toString(number));
-                writer.newLine();
+    private static void findPrimeNumbers(ArrayList<Integer> primeNumbers, String filePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                int number = Integer.parseInt(line);
+                if (isPrime(number)) {
+                    primeNumbers.add(number);
+                }
             }
-            System.out.println("Факториал числа записаны в данный файл:" + filePath);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void findPrimeNumbers() {}
+    private static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void fillNumbersToFile(String path, ArrayList<Integer> numbers) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            for (int number: numbers) {
+                writer.write(Integer.toString(number));
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
