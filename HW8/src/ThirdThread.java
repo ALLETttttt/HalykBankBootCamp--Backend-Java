179475dc5d2e0643f357f20144bb71bdd6660bac
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-11-12 14:12:45 +0600

    SIUUUU

--- HW8/src/ThirdThread.java
+++ HW8/src/ThirdThread.java
@@ -1,5 +1,35 @@
-public class ThirdThread {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class ThirdThread {
+
+    private static final int LIST_SIZE = 22;
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        List<Double> doubleList = new ArrayList<>();
+
+        Thread fillRandomNumbersThread = new Thread(() -> fillRandomNumbers(integerList, doubleList));
+        fillRandomNumbersThread.start();
+
+        try {
+            fillRandomNumbersThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void fillRandomNumbers(List<Integer> integerList, List<Double> doubleList) {
+        Random random = new Random();
+        for (int i = 0; i < LIST_SIZE; i++) {
+            int integers = random.nextInt(100);
+            double doubles = random.nextDouble() * 100;
+
+            integerList.add(integers);
+            doubleList.add(doubles);
+        }
+        System.out.println("Коллекции с целыми числами: " + integerList);
+        System.out.println("Коллекции с вещественными числами: " + doubleList);
     }
 }
