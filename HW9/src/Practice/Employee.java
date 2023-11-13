19a7943d4dc199244f768abe420a844655e6d2bf
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2023-11-13 21:25:26 +0600

    SIUUUU

---
+++ HW9/src/Practice/Employee.java
@@ -0,0 +1,24 @@
+package Practice;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    String firstName;
+    String lastName;
+    int age;
+
+    public Employee(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
