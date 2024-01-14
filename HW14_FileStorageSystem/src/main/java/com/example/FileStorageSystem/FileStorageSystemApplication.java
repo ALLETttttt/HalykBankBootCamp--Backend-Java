b77998489e110ab7150dbf62fae7993631a3f5ea
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2024-01-14 22:40:58 +0600

    SIUUU

--- HW14_FileStorageSystem/src/main/java/com/example/FileStorageSystem/FileStorageSystemApplication.java
+++ HW14_FileStorageSystem/src/main/java/com/example/FileStorageSystem/FileStorageSystemApplication.java
@@ -1,13 +1,13 @@
-package com.example.FileSystem;
+package com.example.FileStorageSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FileSystemApplication {
+public class FileStorageSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FileSystemApplication.class, args);
+		SpringApplication.run(FileStorageSystemApplication.class, args);
 	}
 
 }
