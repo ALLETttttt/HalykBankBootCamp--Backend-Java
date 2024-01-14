a1c70c8cafd9cbe428141a4b222d5d2040e5861f
Бакдаулет <bakdaulet@MacBook-Pro-Bakdaulet.local>
2024-01-14 23:47:58 +0600

    siuuuu

--- HW14_FileStorageSystem/src/main/java/com/example/FileStorageSystem/service/impl/FileStorageServiceImpl.java
+++ HW14_FileStorageSystem/src/main/java/com/example/FileStorageSystem/service/impl/FileStorageServiceImpl.java
@@ -2,19 +2,28 @@ package com.example.FileStorageSystem.service.impl;
 
 import com.example.FileStorageSystem.service.FileStorageService;
 import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+@Service
 public class FileStorageServiceImpl implements FileStorageService {
 
     private final Path root = Paths.get("uploads");
 
     @Override
     public void init() {
-
+        try {
+            Files.createDirectories(root);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize folder for upload!");
+        }
     }
 
     @Override
@@ -29,7 +38,7 @@ public class FileStorageServiceImpl implements FileStorageService {
 
     @Override
     public void deleteAll() {
-
+        FileSystemUtils.deleteRecursively(root.toFile());
     }
 
     @Override
